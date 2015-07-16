e3bb74b0a26e4ce3b484bf5cffeed0a831ce7d0b
Gusdor <gusdor@gmail.com>
2015-07-16 11:26:26 +0100

    First commit

---
+++ Source/ChimetDataAnalysis/ChimetDataProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ChimetDataAnalysis
+{
+    class ChimetDataProvider
+    {
+        public async Task<Dictionary<string, IEnumerable<double>>> DownloadData(DateTime day)
+        {
+            WebRequest dateRequest = HttpWebRequest.Create(string.Format("http://www.sotonmet.co.uk/archive/2015/July/CSV/Sot{0:ddMMMyyyy}.csv", day));
+
+            WebResponse data = await dateRequest.GetResponseAsync();
+
+            using (var reader = new System.IO.StreamReader(data.GetResponseStream()))
+            {
+                string content = await reader.ReadToEndAsync();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Source/ChimetDataAnalysis/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChimetDataAnalysis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.PopulateData();
+        }
+
+        private async void PopulateData()
+        {
+            await this.DownloadData(DateTime.Now.AddDays(-1));
+        }
+
+        private async Task DownloadData(DateTime day)
+        {
+            ChimetDataProvider dataDownloader = new ChimetDataProvider();
+
+            var data = await dataDownloader.DownloadData(day);
+        }
+    }
+}
